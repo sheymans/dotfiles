fd8517e1b675f9d850656f13b2fa48cbec92bac8
Stijn Heymans <stijn.heymans@gmail.com>
2023-12-10 12:36:26 -0800

    Update remap.lua

--- nvim/lua/sheymans/remap.lua
+++ nvim/lua/sheymans/remap.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>t", function() RunRailsTest() end)
+
+-- zz puts you in middle of page when moving up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
