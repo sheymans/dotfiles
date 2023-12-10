74387af15d466d55ddfda14002b7d0c0b2ee7000
Stijn Heymans <stijn.heymans@gmail.com>
2023-12-10 12:40:57 -0800

    Update telescope.lua

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,8 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- pf for project files (for example also node_modules)
-vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- open files only from git
-vim.keymap.set('n', '<leader>ps', function() -- ps for project search
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- pf for project files (like also node_modules)
+vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- search only files in git
+vim.keymap.set('n', '<leader>ps', function() -- ps for project search 
 	builtin.grep_string({ search = vim.fn.input(" Grep > ") });
 end)
 
-vim.keymap.set('n', '<leader>psl', builtin.live_grep, {}) -- project search where you can define your own grep args
-
+vim.keymap.set('n', '<leader>psl', builtin.live_grep, {}) -- project search live
