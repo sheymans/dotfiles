544c5ceb3a566ce0c7966835b3fd7c84d5b57876
Stijn Heymans <stijn.heymans@gmail.com>
2023-12-10 12:37:54 -0800

    Update set.lua

--- nvim/lua/sheymans/set.lua
+++ nvim/lua/sheymans/set.lua
@@ -1,4 +1,12 @@
 vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
 
 vim.opt.smartindent = true
 
