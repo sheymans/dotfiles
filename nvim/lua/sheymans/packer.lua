abed7164a8777dcb37f743f604a0a24df4ee1671
Stijn Heymans <stijn.heymans@gmail.com>
2023-12-10 12:35:12 -0800

    Update packer.lua

--- nvim/lua/sheymans/packer.lua
+++ nvim/lua/sheymans/packer.lua
@@ -1,6 +1,9 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Since we're installing this to spin make sure we have packer installed on each spin instance
 local ensure_packer = function()
 	local fn = vim.fn
-	local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 	if fn.empty(fn.glob(install_path)) > 0 then
 		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 		vim.cmd [[packadd packer.nvim]]
@@ -12,21 +15,22 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
+	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
 		requires = { {'nvim-lua/plenary.nvim'} }
 	}
-
-	use {'briones-gabriel/darcula-solid.nvim', requires = 'rktjmp/lush.nvim' }
+	use { "briones-gabriel/darcula-solid.nvim", requires = "rktjmp/lush.nvim" }
 	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-	use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-	use {
-		"ThePrimeagen/harpoon",
-		branch = "harpoon2",
-		requires = { {"nvim-lua/plenary.nvim"} }
-	}
+  use('nvim-lua/plenary.nvim')
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  }
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
 
@@ -37,14 +41,16 @@ return require('packer').startup(function(use)
 			{'williamboman/mason.nvim'},
 			{'williamboman/mason-lspconfig.nvim'},
 
+			-- LSP Support
 			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
 			{'hrsh7th/nvim-cmp'},
 			{'hrsh7th/cmp-nvim-lsp'},
 			{'L3MON4D3/LuaSnip'},
 		}
 	}
-
 	use('github/copilot.vim')
+	use('Shopify/spin-hud')
 
 	use {
 		'lewis6991/gitsigns.nvim',
@@ -52,6 +58,11 @@ return require('packer').startup(function(use)
 			require('gitsigns').setup()
 		end
 	}
+
+	use('nvim-tree/nvim-web-devicons')
+	use('sindrets/diffview.nvim')
+
+  use('nvim-treesitter/nvim-treesitter-context')
 
 	if packer_bootstrap then
 		require('packer').sync()
