39f1f5ca54919f9f73cd979df961f24667d053c2
Stijn Heymans <stijn.heymans@gmail.com>
2024-09-08 12:42:00 -0700

    Update init.lua

--- nvim/lua/sheymans/init.lua
+++ nvim/lua/sheymans/init.lua
@@ -1,7 +1,11 @@
-require("sheymans.packer")
+require("sheymans.lazy")
 require("sheymans.remap")
 require("sheymans.set")
-require("sheymans.railstest")
 
 vim.cmd 'colorscheme darcula-solid'
 vim.cmd 'set termguicolors'
+
+vim.api.nvim_set_hl(0, "LineNr", { bg = "#f56942", fg = "#cfcfcf" })
+vim.api.nvim_set_hl(0, "LineNrAbove", {fg = "#ebb734" })
+vim.api.nvim_set_hl(0, "LineNrBelow", {fg = "#ebb734" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "#262627" })
