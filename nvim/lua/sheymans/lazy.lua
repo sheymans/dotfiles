bc9c57622115eee84b738f78cfc71178748fd1f9
Stijn Heymans <stijn.heymans@gmail.com>
2024-09-08 12:43:28 -0700

    Create lazy.lua

---
+++ nvim/lua/sheymans/lazy.lua
@@ -0,0 +1,72 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	'wbthomason/packer.nvim',
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.4',
+		-- or                            , branch = '0.1.x',
+		dependencies = { {'nvim-lua/plenary.nvim'} }
+	},
+	{ "briones-gabriel/darcula-solid.nvim", dependencies = "rktjmp/lush.nvim" },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  'nvim-lua/plenary.nvim',
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { {"nvim-lua/plenary.nvim"} }
+  },
+	'tpope/vim-fugitive',
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		dependencies = {
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	},
+	'github/copilot.vim',
+	{
+		'lewis6991/gitsigns.nvim',
+		config = function()
+			require('gitsigns').setup()
+		end
+	},
+	'nvim-tree/nvim-web-devicons',
+	'sindrets/diffview.nvim',
+  'nvim-treesitter/nvim-treesitter-context',
+{
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "echasnovski/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+}
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
