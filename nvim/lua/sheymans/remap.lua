02274aa425797320a33fd1f32f69fadb90dc7f93
Stijn Heymans <stijn.heymans@gmail.com>
2024-10-04 16:41:12 -0700

    Update remap.lua

--- nvim/lua/sheymans/remap.lua
+++ nvim/lua/sheymans/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 vim.keymap.set("n", "<leader>t", function() RunRailsTest() end)
 
@@ -8,3 +8,12 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+vim.keymap.set({"n", "v"}, "gh", function() CopyGithubURL() end)
+vim.keymap.set({"n", "v"}, "<leader>gh", function() CopyGithubURL(true) end)
+
+-- use tab to loop through splits
+vim.keymap.set("n", "<Tab>", "<C-w>w")
+
+-- copy relative path of current file to clipboard
+vim.keymap.set("n", "<leader>cf", ':let @*=expand("%")<CR>')
